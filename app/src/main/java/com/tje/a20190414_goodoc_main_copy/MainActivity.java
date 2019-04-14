dd8806f3169bc0e318e06e81f1b562dbba385b42
unnato0159 <unnato01@naver.com>
2019-04-14 14:21:47 +0900

    메인화면 버튼 클릭까지 구성

--- app/src/main/java/com/tje/a20190414_goodoc_main_copy/MainActivity.java
+++ app/src/main/java/com/tje/a20190414_goodoc_main_copy/MainActivity.java
@@ -2,12 +2,35 @@ package com.tje.a20190414_goodoc_main_copy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText search;
+    Button searchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search = findViewById(R.id.search);
+        searchBtn = findViewById(R.id.searchBtn);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String searchData = search.getText().toString();
+                String searchData1 = String.format("[%s]을 검색합니다.",searchData);
+
+                Toast.makeText(MainActivity.this, searchData, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, searchData1, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
